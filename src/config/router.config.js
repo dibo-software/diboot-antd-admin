4ad304034d52d7e3359a4e95389fad9c7c673e50
Zhaoyang <yangzhao635226@163.com>
2020-03-02 10:03:50 +0800

    页面不显示出异常页相关菜单

--- src/config/router.config.js
+++ src/config/router.config.js
@@ -105,25 +105,26 @@ export const asyncRouterMap = [
         name: 'exception',
         component: RouteView,
         redirect: '/exception/403',
-        meta: { title: '异常页', icon: 'warning', permission: [ 'exception' ] },
+        hidden: true,
+        meta: { title: '异常页', icon: 'warning' },
         children: [
           {
             path: '/exception/403',
             name: 'Exception403',
             component: () => import(/* webpackChunkName: "fail" */ '@/views/exception/403'),
-            meta: { title: '403', permission: [ 'exception' ] }
+            meta: { title: '403' }
           },
           {
             path: '/exception/404',
             name: 'Exception404',
             component: () => import(/* webpackChunkName: "fail" */ '@/views/exception/404'),
-            meta: { title: '404', permission: [ 'exception' ] }
+            meta: { title: '404' }
           },
           {
             path: '/exception/500',
             name: 'Exception500',
             component: () => import(/* webpackChunkName: "fail" */ '@/views/exception/500'),
-            meta: { title: '500', permission: [ 'exception' ] }
+            meta: { title: '500' }
           }
         ]
       }
