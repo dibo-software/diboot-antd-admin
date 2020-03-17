e72b8819215a087d2799e2c9407c65bbdd66cb1d
Zhaoyang <yangzhao635226@163.com>
2020-03-17 16:35:44 +0800

    更改后端接口为8080

--- vue.config.js
+++ vue.config.js
@@ -93,11 +93,10 @@ module.exports = {
 
   devServer: {
     // development server port 8000
-    port: 8005,
+    port: 8000,
     proxy: {
       '/api': {
-        // target: 'https://mock.ihx.me/mock/5baf3052f7da7e07e04a5116/antd-pro',
-        target: 'http://localhost:8085',
+        target: 'http://localhost:8080',
         ws: true,
         changeOrigin: true,
         pathRewrite: {
