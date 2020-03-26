e168383ae42bc46a70a3a625bf4e868320efd1fc
Zhaoyang <yangzhao635226@163.com>
2020-03-26 09:53:13 +0800

    登录日志列表显示出每次的登录状态

--- src/views/system/iamLoginTrace/list.vue
+++ src/views/system/iamLoginTrace/list.vue
@@ -14,14 +14,17 @@
             </a-form-item>
           </a-col>
           <a-col :md="6" :sm="24">
-            <a-form-item label="登录方式">
-              <a-select placeholder="请选择" default-value="" v-model="queryParam.authType">
+            <a-form-item label="登录状态">
+              <a-select placeholder="请选择" default-value="" v-model="queryParam.success">
                 <a-select-option
-                  v-for="(authType, index) in more.authTypeKvList"
-                  :key="index"
-                  :value="authType.v"
+                  :value="true"
                 >
-                  {{ authType.k }}
+                  成功
+                </a-select-option>
+                <a-select-option
+                  :value="false"
+                >
+                  失败
                 </a-select-option>
               </a-select>
             </a-form-item>
@@ -52,6 +55,10 @@
       @change="handleTableChange"
       rowKey="id"
     >
+      <span slot="successLabel" slot-scope="text, record">
+        <a-tag v-if="text === true" color="green">成功</a-tag>
+        <a-tag v-else color="red">失败</a-tag>
+      </span>
       <span slot="roleList" slot-scope="text, record">
         <template v-if="record.roleList">
           <a-tag color="cyan" v-for="(role, index) in record.roleList" :key="index">{{ role.name }}</a-tag>
@@ -97,6 +104,11 @@ export default {
         {
           title: '登录方式',
           dataIndex: 'authTypeLabel'
+        },
+        {
+          title: '登录状态',
+          dataIndex: 'success',
+          scopedSlots: { customRender: 'successLabel' }
         },
         {
           title: '登录时间',
