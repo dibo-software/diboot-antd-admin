d1be380a5b8e62fa537de9de236483d0b4eedcf4
Zhaoyang <yangzhao635226@163.com>
2020-06-13 12:21:03 +0800

    增加v-viewer和图片预览功能

--- src/main.js
+++ src/main.js
@@ -7,6 +7,9 @@ import router from './router'
 import store from './store/'
 import { VueAxios } from './utils/request'
 
+import Viewer from 'v-viewer'
+import 'viewerjs/dist/viewer.css'
+
 // mock
 // import './mock'
 
@@ -19,6 +22,11 @@ Vue.config.productionTip = false
 
 // mount axios Vue.$http and this.$http
 Vue.use(VueAxios)
+
+Vue.use(Viewer)
+Viewer.setDefaults({
+  Options: { 'inline': true, 'button': true, 'navbar': true, 'title': true, 'toolbar': true, 'tooltip': true, 'movable': true, 'zoomable': true, 'rotatable': true, 'scalable': true, 'transition': true, 'fullscreen': true, 'keyboard': true, 'url': 'data-source' }
+})
 
 new Vue({
   router,
