3a1e72f7f720a92cb60268a95494bf928c3f20d2
wuy <1311695042@qq.com>
2020-06-30 00:27:23 +0800

    modify:修改上传组件

--- src/components/diboot/mixins/detail.js
+++ src/components/diboot/mixins/detail.js
@@ -28,7 +28,26 @@ export default {
       }
     },
     downloadFile (path) {
-      window.location.href = path
+      dibootApi.download(path)
+        .then(res => {
+          const blob = new Blob([res.data])
+          if ('download' in document.createElement('a')) {
+            // 非IE下载
+            const elink = document.createElement('a')
+            elink.download = res.filename
+            elink.style.display = 'none'
+            elink.href = URL.createObjectURL(blob)
+            document.body.appendChild(elink)
+            elink.click()
+            URL.revokeObjectURL(elink.href) // 释放URL 对象
+            document.body.removeChild(elink)
+          } else {
+            // IE10+下载
+            navigator.msSaveBlob(blob, res.filename)
+          }
+        }).catch(err => {
+          console.log(err)
+        })
     },
     close () {
       this.visible = false
