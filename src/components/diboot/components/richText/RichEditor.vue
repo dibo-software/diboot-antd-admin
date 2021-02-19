484c239338458382a58274c4daf8aab15a677a3b
yangzhao635226@163.com <zhaozhaoyang123456>
2021-02-19 12:55:35 +0800

    + tinymce 富文本编辑器； - quill与wang富文本编辑器； optimize: 业务中引用通用的RichEditor与RichRender组件，方便自定义替换。

---
+++ src/components/diboot/components/richText/RichEditor.vue
@@ -0,0 +1,24 @@
+<template>
+  <div>
+    <tinymce :value="value" @input="val => $emit('change', val)"></tinymce>
+  </div>
+</template>
+<script>
+  import tinymce from '@/components/Editor/Tinymce/index'
+  export default {
+    name: 'RichEditor',
+    components: {
+      tinymce
+    },
+    model: {
+      prop: 'value',
+      event: 'change'
+    },
+    props: {
+      value: {
+        type: String,
+        default: ''
+      }
+    }
+  }
+</script>
---
+++ src/components/diboot/components/richText/RichRender.vue
@@ -0,0 +1,18 @@
+<template>
+  <div v-html="content"></div>
+</template>
+<script>
+  export default {
+    name: 'RichRender',
+    model: {
+      prop: 'content',
+      event: 'change'
+    },
+    props: {
+      content: {
+        type: String,
+        default: ''
+      }
+    }
+  }
+</script>
