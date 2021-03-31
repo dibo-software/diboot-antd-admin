f315a568750d2709b74d58102a92668dc94bfc71
yangzhao635226@163.com <zhaozhaoyang123456>
2021-03-31 14:20:40 +0800

    fix: 偶发的富文本不能输入的问题

--- src/components/Editor/Tinymce/index.vue
+++ src/components/Editor/Tinymce/index.vue
@@ -5,256 +5,259 @@
 </template>
 
 <script>
-  /**
-   * docs:
-   * https://panjiachen.github.io/vue-element-admin-site/feature/component/rich-editor.html#tinymce
-   */
-  import plugins from './plugins'
-  import toolbar from './toolbar'
-  import load from './dynamicLoadScript'
-  import { baseURL, dibootApi } from '@/utils/request'
+/**
+ * docs:
+ * https://panjiachen.github.io/vue-element-admin-site/feature/component/rich-editor.html#tinymce
+ */
+import plugins from './plugins'
+import toolbar from './toolbar'
+import load from './dynamicLoadScript'
+import { baseURL, dibootApi } from '@/utils/request'
 
-  const tinymceCDN = 'https://cdn.jsdelivr.net/npm/tinymce@5.7.0/tinymce.min.js'
+const tinymceCDN = 'https://cdn.jsdelivr.net/npm/tinymce@5.7.0/tinymce.min.js'
 
-  export default {
-    name: 'Tinymce',
-    props: {
-      id: {
-        type: String,
-        default: function () {
-          return 'vue-tinymce-' + +new Date() + ((Math.random() * 1000).toFixed(0) + '')
-        }
-      },
-      value: {
-        type: String,
-        default: ''
-      },
-      toolbar: {
-        type: Array,
-        required: false,
-        default () {
-          return []
-        }
-      },
-      menubar: {
-        type: String,
-        default: 'file edit insert view format table'
-      },
-      height: {
-        type: [Number, String],
-        required: false,
-        default: 480
-      },
-      width: {
-        type: [Number, String],
-        required: false,
-        default: 'auto'
-      },
-      relObjType: {
-        type: String,
-        required: true
-      },
-      relObjId: {
-        type: [Number, String],
-        required: false,
-        default: ''
+export default {
+  name: 'Tinymce',
+  props: {
+    id: {
+      type: String,
+      default: function () {
+        return 'vue-tinymce-' + +new Date() + ((Math.random() * 1000).toFixed(0) + '')
       }
     },
-    data () {
-      return {
-        hasChange: false,
-        hasInit: false,
-        tinymceId: this.id,
-        fullscreen: false,
-        changeCounter: 0
+    value: {
+      type: String,
+      default: ''
+    },
+    toolbar: {
+      type: Array,
+      required: false,
+      default () {
+        return []
       }
     },
-    computed: {
-      containerWidth () {
-        const width = this.width
-        if (/^[\d]+(\.[\d]+)?$/.test(width)) { // matches `100`, `'100'`
-          return `${width}px`
-        }
-        return width
+    menubar: {
+      type: String,
+      default: 'file edit insert view format table'
+    },
+    height: {
+      type: [Number, String],
+      required: false,
+      default: 480
+    },
+    width: {
+      type: [Number, String],
+      required: false,
+      default: 'auto'
+    },
+    relObjType: {
+      type: String,
+      required: true
+    },
+    relObjId: {
+      type: [Number, String],
+      required: false,
+      default: ''
+    }
+  },
+  data () {
+    return {
+      hasChange: false,
+      hasInit: false,
+      tinymceId: this.id,
+      fullscreen: false,
+      changeCounter: 0
+    }
+  },
+  computed: {
+    containerWidth () {
+      const width = this.width
+      if (/^[\d]+(\.[\d]+)?$/.test(width)) { // matches `100`, `'100'`
+        return `${width}px`
       }
-    },
-    watch: {
-      value: {
-        handler: function (val) {
-          if (!this.hasChange && this.hasInit) {
-            this.$nextTick(() => {
-              window.tinymce.get(this.tinymceId).setContent(val || '')
-            })
-          }
+      return width
+    }
+  },
+  watch: {
+    value: {
+      handler: function (val) {
+        if (!this.hasChange && this.hasInit) {
+          this.$nextTick(() => {
+            window.tinymce.get(this.tinymceId).setContent(val || '')
+          })
         }
-      }
-    },
-    mounted () {
-      this.init()
-    },
-    activated () {
-      if (window.tinymce) {
-        this.initTinymce()
-      }
-    },
-    deactivated () {
-      this.destroyTinymce()
-    },
-    destroyed () {
-      this.destroyTinymce()
-    },
-    methods: {
-      init () {
-        // dynamic load tinymce from cdn
-        load(tinymceCDN, (err) => {
-          if (err) {
-            this.$message.error(err.message)
-            return
-          }
-          this.initTinymce()
-        })
-      },
-      initTinymce () {
-        const _this = this
-        let publicBaseUrl = process.env.BASE_URL
-        if (publicBaseUrl && publicBaseUrl.endsWith('/')) {
-          publicBaseUrl = publicBaseUrl.substring(0, publicBaseUrl.length - 1)
-        }
-        window.tinymce.init({
-          selector: `#${this.tinymceId}`,
-          language_url: `${publicBaseUrl}/tinymce/language/zh_CN.js`,
-          language: 'zh_CN',
-          height: _this.height,
-          resize: true,
-          body_class: 'panel-body ',
-          object_resizing: false,
-          toolbar: this.toolbar.length > 0 ? this.toolbar : toolbar,
-          menubar: this.menubar,
-          plugins: plugins,
-          end_container_on_empty_block: true,
-          powerpaste_word_import: 'clean',
-          code_dialog_height: 450,
-          code_dialog_width: 1000,
-          advlist_bullet_styles: 'square',
-          advlist_number_styles: 'default',
-          default_link_target: '_blank',
-          link_title: false,
-          fontsize_formats: '12px 14px 16px 18px 24px 36px 48px 56px 72px',
-          font_formats: '微软雅黑=Microsoft YaHei,Helvetica Neue,PingFang SC,sans-serif;苹果苹方=PingFang SC,Microsoft YaHei,sans-serif;宋体=simsun,serif;仿宋体=FangSong,serif;黑体=SimHei,sans-serif;Arial=arial,helvetica,sans-serif;Arial Black=arial black,avant garde;Book Antiqua=book antiqua,palatino;Comic Sans MS=comic sans ms,sans-serif;Courier New=courier new,courier;Georgia=georgia,palatino;Helvetica=helvetica;Impact=impact,chicago;Symbol=symbol;Tahoma=tahoma,arial,helvetica,sans-serif;Terminal=terminal,monaco;Times New Roman=times new roman,times;Verdana=verdana,geneva;Webdings=webdings;Wingdings=wingdings,zapf dingbats;知乎配置=BlinkMacSystemFont, Helvetica Neue, PingFang SC, Microsoft YaHei, Source Han Sans SC, Noto Sans CJK SC, WenQuanYi Micro Hei, sans-serif;小米配置=Helvetica Neue,Helvetica,Arial,Microsoft Yahei,Hiragino Sans GB,Heiti SC,WenQuanYi Micro Hei,sans-serif',
-          nonbreaking_force_tab: true, // inserting nonbreaking space &nbsp; need Nonbreaking Space Plugin
-          init_instance_callback: editor => {
-            if (_this.value) {
-              editor.setContent(_this.value)
-            }
-            _this.hasInit = true
-            editor.on('NodeChange Change KeyUp SetContent', () => {
-              if (_this.changeCounter > 0) {
-                _this.hasChange = true
-                _this.$emit('input', editor.getContent())
-              }
-              _this.changeCounter++
-            })
-          },
-          setup (editor) {
-            editor.on('FullscreenStateChanged', (e) => {
-              _this.fullscreen = e.state
-            })
-          },
-          // it will try to keep these URLs intact
-          // https://www.tiny.cloud/docs-3x/reference/configuration/Configuration3x@convert_urls/
-          // https://stackoverflow.com/questions/5196205/disable-tinymce-absolute-to-relative-url-conversions
-          convert_urls: false,
-          images_upload_handler (blobInfo, successFunc, failFunc, progress) {
-            // 将文件blob转换为formData
-            const file = blobInfo.blob()
-            const formData = new FormData()
-            formData.append('file', file, file.name)
-            // 添加上传图片的附加属性
-            formData.append('relObjType', _this.relObjType)
-            formData.append('relObjId', _this.relObjId)
-            formData.append('relObjField', 'rich_text')
-            dibootApi.upload('/uploadFile/upload', formData).then(res => {
-              if (res.code === 0) {
-                successFunc(`${baseURL}${res.data.accessUrl}/image`)
-              } else {
-                failFunc(res.msg)
-              }
-            }).catch(() => {
-              failFunc('上传图片出错，请重试')
-            })
-          }
-
-          // 整合七牛上传
-          // images_dataimg_filter(img) {
-          //   setTimeout(() => {
-          //     const $image = $(img);
-          //     $image.removeAttr('width');
-          //     $image.removeAttr('height');
-          //     if ($image[0].height && $image[0].width) {
-          //       $image.attr('data-wscntype', 'image');
-          //       $image.attr('data-wscnh', $image[0].height);
-          //       $image.attr('data-wscnw', $image[0].width);
-          //       $image.addClass('wscnph');
-          //     }
-          //   }, 0);
-          //   return img
-          // },
-          // images_upload_handler(blobInfo, success, failure, progress) {
-          //   progress(0);
-          //   const token = _this.$store.getters.token;
-          //   getToken(token).then(response => {
-          //     const url = response.data.qiniu_url;
-          //     const formData = new FormData();
-          //     formData.append('token', response.data.qiniu_token);
-          //     formData.append('key', response.data.qiniu_key);
-          //     formData.append('file', blobInfo.blob(), url);
-          //     upload(formData).then(() => {
-          //       success(url);
-          //       progress(100);
-          //     })
-          //   }).catch(err => {
-          //     failure('出现未知问题，刷新页面，或者联系程序员')
-          //     console.log(err);
-          //   });
-          // },
-        })
-      },
-      destroyTinymce () {
-        const tinymce = window.tinymce.get(this.tinymceId)
-        if (this.fullscreen) {
-          tinymce.execCommand('mceFullScreen')
-        }
-
-        if (tinymce) {
-          tinymce.destroy()
-        }
-      },
-      setContent (value) {
-        window.tinymce.get(this.tinymceId).setContent(value)
-      },
-      getContent () {
-        window.tinymce.get(this.tinymceId).getContent()
       }
     }
+  },
+  mounted () {
+    this.$nextTick(function () {
+      this.init()
+    })
+  },
+  activated () {
+    if (window.tinymce) {
+      this.initTinymce()
+    }
+  },
+  deactivated () {
+    this.destroyTinymce()
+  },
+  destroyed () {
+    this.destroyTinymce()
+  },
+  methods: {
+    init () {
+      // dynamic load tinymce from cdn
+      load(tinymceCDN, (err) => {
+        if (err) {
+          this.$message.error(err.message)
+          return
+        }
+        this.initTinymce()
+      })
+    },
+    initTinymce () {
+      const _this = this
+      // eslint-disable-next-line no-undef
+      let publicBaseUrl = process.env.BASE_URL
+      if (publicBaseUrl && publicBaseUrl.endsWith('/')) {
+        publicBaseUrl = publicBaseUrl.substring(0, publicBaseUrl.length - 1)
+      }
+      window.tinymce.init({
+        selector: `#${this.tinymceId}`,
+        language_url: `${publicBaseUrl}/tinymce/language/zh_CN.js`,
+        language: 'zh_CN',
+        height: _this.height,
+        resize: true,
+        body_class: 'panel-body ',
+        object_resizing: false,
+        toolbar: this.toolbar.length > 0 ? this.toolbar : toolbar,
+        menubar: this.menubar,
+        plugins: plugins,
+        end_container_on_empty_block: true,
+        powerpaste_word_import: 'clean',
+        code_dialog_height: 450,
+        code_dialog_width: 1000,
+        advlist_bullet_styles: 'square',
+        advlist_number_styles: 'default',
+        default_link_target: '_blank',
+        link_title: false,
+        fontsize_formats: '12px 14px 16px 18px 24px 36px 48px 56px 72px',
+        font_formats: '微软雅黑=Microsoft YaHei,Helvetica Neue,PingFang SC,sans-serif;苹果苹方=PingFang SC,Microsoft YaHei,sans-serif;宋体=simsun,serif;仿宋体=FangSong,serif;黑体=SimHei,sans-serif;Arial=arial,helvetica,sans-serif;Arial Black=arial black,avant garde;Book Antiqua=book antiqua,palatino;Comic Sans MS=comic sans ms,sans-serif;Courier New=courier new,courier;Georgia=georgia,palatino;Helvetica=helvetica;Impact=impact,chicago;Symbol=symbol;Tahoma=tahoma,arial,helvetica,sans-serif;Terminal=terminal,monaco;Times New Roman=times new roman,times;Verdana=verdana,geneva;Webdings=webdings;Wingdings=wingdings,zapf dingbats;知乎配置=BlinkMacSystemFont, Helvetica Neue, PingFang SC, Microsoft YaHei, Source Han Sans SC, Noto Sans CJK SC, WenQuanYi Micro Hei, sans-serif;小米配置=Helvetica Neue,Helvetica,Arial,Microsoft Yahei,Hiragino Sans GB,Heiti SC,WenQuanYi Micro Hei,sans-serif',
+        nonbreaking_force_tab: true, // inserting nonbreaking space &nbsp; need Nonbreaking Space Plugin
+        init_instance_callback: editor => {
+          if (_this.value) {
+            editor.setContent(_this.value)
+          }
+          _this.hasInit = true
+          editor.on('NodeChange Change KeyUp SetContent', () => {
+            if (_this.changeCounter > 0) {
+              _this.hasChange = true
+              _this.$emit('input', editor.getContent())
+            }
+            _this.changeCounter++
+          })
+        },
+        setup (editor) {
+          editor.on('FullscreenStateChanged', (e) => {
+            _this.fullscreen = e.state
+          })
+        },
+        // it will try to keep these URLs intact
+        // https://www.tiny.cloud/docs-3x/reference/configuration/Configuration3x@convert_urls/
+        // https://stackoverflow.com/questions/5196205/disable-tinymce-absolute-to-relative-url-conversions
+        convert_urls: false,
+        images_upload_handler (blobInfo, successFunc, failFunc, progress) {
+          // 将文件blob转换为formData
+          const file = blobInfo.blob()
+          const formData = new FormData()
+          formData.append('file', file, file.name)
+          // 添加上传图片的附加属性
+          formData.append('relObjType', _this.relObjType)
+          formData.append('relObjId', _this.relObjId)
+          formData.append('relObjField', 'rich_text')
+          dibootApi.upload('/uploadFile/upload', formData).then(res => {
+            if (res.code === 0) {
+              successFunc(`${baseURL}${res.data.accessUrl}/image`)
+            } else {
+              failFunc(res.msg)
+            }
+          }).catch(() => {
+            failFunc('上传图片出错，请重试')
+          })
+        }
+
+        // 整合七牛上传
+        // images_dataimg_filter(img) {
+        //   setTimeout(() => {
+        //     const $image = $(img);
+        //     $image.removeAttr('width');
+        //     $image.removeAttr('height');
+        //     if ($image[0].height && $image[0].width) {
+        //       $image.attr('data-wscntype', 'image');
+        //       $image.attr('data-wscnh', $image[0].height);
+        //       $image.attr('data-wscnw', $image[0].width);
+        //       $image.addClass('wscnph');
+        //     }
+        //   }, 0);
+        //   return img
+        // },
+        // images_upload_handler(blobInfo, success, failure, progress) {
+        //   progress(0);
+        //   const token = _this.$store.getters.token;
+        //   getToken(token).then(response => {
+        //     const url = response.data.qiniu_url;
+        //     const formData = new FormData();
+        //     formData.append('token', response.data.qiniu_token);
+        //     formData.append('key', response.data.qiniu_key);
+        //     formData.append('file', blobInfo.blob(), url);
+        //     upload(formData).then(() => {
+        //       success(url);
+        //       progress(100);
+        //     })
+        //   }).catch(err => {
+        //     failure('出现未知问题，刷新页面，或者联系程序员')
+        //     console.log(err);
+        //   });
+        // },
+      })
+    },
+    destroyTinymce () {
+      const tinymce = window.tinymce.get(this.tinymceId)
+      if (this.fullscreen) {
+        tinymce.execCommand('mceFullScreen')
+      }
+
+      if (tinymce) {
+        tinymce.destroy()
+      }
+    },
+    setContent (value) {
+      window.tinymce.get(this.tinymceId).setContent(value)
+    },
+    getContent () {
+      window.tinymce.get(this.tinymceId).getContent()
+    }
   }
+}
 </script>
 
 <style lang="less" scoped>
-  .tinymce-container {
-    position: relative;
-    line-height: normal;
-  }
+.tinymce-container {
+  position: relative;
+  line-height: normal;
+}
 
-  .tinymce-container {
-    ::v-deep {
-      .mce-fullscreen {
-        z-index: 10000;
-      }
+.tinymce-container {
+  ::v-deep {
+    .mce-fullscreen {
+      z-index: 10000;
     }
   }
+}
 
-  .tinymce-textarea {
-    visibility: hidden;
-    z-index: -1;
-  }
+.tinymce-textarea {
+  visibility: hidden;
+  z-index: -1;
+}
 </style>
