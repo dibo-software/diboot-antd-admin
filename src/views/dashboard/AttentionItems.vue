278ed6e9ff1882ab1946b4d4ba5e98ef5fccf1c5
wuy <1311695042@qq.com>
2021-05-07 08:46:12 +0800

    add: 初始化注意事项组件

---
+++ src/views/dashboard/AttentionItems.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="AttentionItems">
+    <h3>上线注意示项</h3>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'AttentionItems'
+  }
+</script>
+
+<style scoped lang="less" rel="stylesheet/less" scoped>
+
+</style>
--- src/views/dashboard/Index.vue
+++ src/views/dashboard/Index.vue
@@ -1,14 +1,17 @@
 <template>
   <div class="dashboard">
     <h2>欢迎您，{{ nickname() }}！</h2>
+    <attention-items></attention-items>
   </div>
 </template>
 
 <script>
 import { mapGetters } from 'vuex'
+import AttentionItems from './AttentionItems'
 
 export default {
   name: 'Dashboard',
+  components: { AttentionItems },
   data () {
     return {
     }
