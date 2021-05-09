cfd91290b580d12307467d48eace1a9a7ec1b6ed
wuy <1311695042@qq.com>
2021-05-09 17:55:59 +0800

    add: 增加上线注意事项

--- src/views/dashboard/AttentionItems.vue
+++ src/views/dashboard/AttentionItems.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="AttentionItems">
     <h3>上线注意示项</h3>
-    <iframe src="https://www.diboot.com/guide/diboot-antd-admin/%E4%BB%8B%E7%BB%8D.html" width="100%" height="800px"></iframe>
+    <iframe src="https://www.diboot.com/guide/diboot-antd-admin/%E6%9D%83%E9%99%90%E6%8E%A7%E5%88%B6.html#%E8%8F%9C%E5%8D%95%E6%9D%83%E9%99%90%E7%AE%A1%E7%90%86" width="100%" height="800px"></iframe>
   </div>
 </template>
 
