0e885d827c69253449dedb252c2e3b7c3c885dac
wuy <1311695042@qq.com>
2021-05-10 10:19:08 +0800

    add: 增加上线注意事项

--- src/views/dashboard/Index.vue
+++ src/views/dashboard/Index.vue
@@ -1,19 +1,38 @@
 <template>
   <div class="dashboard">
     <h2>欢迎您，{{ nickname() }}！</h2>
-    <attention-items></attention-items>
+    <a-list :data-source="attentionItems" bordered>
+      <a-list-item slot="renderItem" slot-scope="item, index">
+        <a-list-item-meta
+        >
+          <a slot="title" :href="item.titleUrl" target="_blank" style="color: #1585ff">{{ index + 1 + '.' + item.title }}</a>
+          <template v-if="item.description">
+            <span slot="description">{{ item.description }}</span>
+          </template>
+        </a-list-item-meta>
+      </a-list-item>
+    </a-list>
   </div>
 </template>
 
 <script>
 import { mapGetters } from 'vuex'
-import AttentionItems from './AttentionItems'
 
 export default {
   name: 'Dashboard',
-  components: { AttentionItems },
   data () {
     return {
+      attentionItems: [
+        {
+          title: '权限配置上线注意事项',
+          titleUrl: 'https://www.diboot.com/guide/diboot-antd-admin/%E6%9D%83%E9%99%90%E6%8E%A7%E5%88%B6.html#%E8%8F%9C%E5%8D%95%E6%9D%83%E9%99%90%E7%AE%A1%E7%90%86',
+          description: '描述内容'
+        },
+        {
+          title: '权限配置上线注意事项',
+          titleUrl: 'https://www.diboot.com/guide/diboot-antd-admin/%E6%9D%83%E9%99%90%E6%8E%A7%E5%88%B6.html#%E8%8F%9C%E5%8D%95%E6%9D%83%E9%99%90%E7%AE%A1%E7%90%86'
+        }
+      ]
     }
   },
   created () {
