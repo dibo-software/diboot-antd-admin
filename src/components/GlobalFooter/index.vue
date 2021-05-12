078a3968899150b2e36b7072ac4908353c0c2be3
JerryMa <mazc@dibo.ltd>
2021-05-12 18:27:25 +0800

    * 更新描述文本

--- src/components/GlobalFooter/index.vue
+++ src/components/GlobalFooter/index.vue
@@ -1,10 +1,5 @@
 <template>
   <global-footer class="footer custom-render">
-    <template v-slot:links>
-      <a href="https://www.diboot.com/guide/diboot-antd-admin/%E4%BB%8B%E7%BB%8D.html" target="_blank">Diboot Antd Admin</a>
-      <a href="https://pro.antdv.com/" target="_blank">Ant Design Pro</a>
-      <a href="https://www.antdv.com/docs/vue/introduce-cn/" target="_blank">Ant Design Vue</a>
-    </template>
     <template v-slot:copyright>
       Copyright &copy; 2021 <a
         href="https://www.diboot.com/"
--- src/views/dashboard/Index.vue
+++ src/views/dashboard/Index.vue
@@ -1,18 +1,21 @@
 <template>
   <div class="dashboard">
     <h2>欢迎您，{{ nickname() }}！</h2>
-    <h3>上线注意事项列表</h3>
-    <a-list :data-source="attentionItems" bordered>
-      <a-list-item slot="renderItem" slot-scope="item, index">
-        <a-list-item-meta
-        >
-          <a slot="title" :href="item.titleUrl" target="_blank" style="color: #1585ff">{{ index + 1 + '.' + item.title }}</a>
-          <template v-if="item.description">
-            <span slot="description">{{ item.description }}</span>
-          </template>
-        </a-list-item-meta>
-      </a-list-item>
-    </a-list>
+    <div>
+      <a-card title="帮助向导" :bordered="false">
+        <a-list :data-source="attentionItems" bordered>
+          <a-list-item slot="renderItem" slot-scope="item, index">
+            <a-list-item-meta
+            >
+              <a slot="title" :href="item.titleUrl" target="_blank" style="color: #1585ff">{{ index + 1 + '. ' + item.title }}</a>
+              <template v-if="item.description">
+                <span slot="description">{{ item.description }}</span>
+              </template>
+            </a-list-item-meta>
+          </a-list-item>
+        </a-list>
+      </a-card>
+    </div>
   </div>
 </template>
 
@@ -25,9 +28,31 @@ export default {
     return {
       attentionItems: [
         {
-          title: '权限配置上线注意事项',
+          title: '新手指南',
+          titleUrl: 'https://www.diboot.com/guide/newer/beginner.html',
+          description: 'diboot新用户，先通读官网文档'
+        },
+        {
+          title: '配置角色&资源权限',
           titleUrl: 'https://www.diboot.com/guide/diboot-antd-admin/%E6%9D%83%E9%99%90%E6%8E%A7%E5%88%B6.html#%E8%8F%9C%E5%8D%95%E6%9D%83%E9%99%90%E7%AE%A1%E7%90%86',
-          description: '描述内容'
+          description: '参考链接官网文档，配置资源权限'
+        },
+        {
+          title: '后端打包时剔除devtools',
+          titleUrl: 'https://www.diboot.com/guide/diboot-devtools/开始使用.html',
+          description: 'diboot-devtools 是用于开发过程的助手工具，打包发布前须剔除devtools jar，具体请参考链接官网文档。'
+        },
+        {
+          title: '常用问题FAQ',
+          titleUrl: 'https://www.diboot.com/guide/notes/faq/main.html',
+          description:
+            '使用中有疑问可参考FAQ 或 通过github/gitee查找技术交流群沟通'
+        },
+        {
+          title: 'diboot企业版支持',
+          titleUrl: 'https://www.diboot.com/ent/service.html',
+          description:
+            'devtools高级功能、diboot-cloud微服务版本、diboot-workflow工作流、diboot-mobile移动端等可查看授权服务相关内容'
         }
       ]
     }
