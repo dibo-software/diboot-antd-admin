c1ea194ecf5a2bcf4f6e2465b22c6536831a058d
wind <1103642893@qq.com>
2021-12-28 18:55:59 +0800

    * 简化级联方案

--- src/components/diboot/mixins/more.js
+++ src/components/diboot/mixins/more.js
@@ -12,7 +12,9 @@ export default {
       // 远程过滤加载状态
       attachMoreLoading: false,
       // 关联相关的更多数据
-      more: {}
+      more: {},
+      // 控制，用于级联控制选择的选择加载控制
+      control: {}
     }
   },
   methods: {
@@ -41,7 +43,7 @@ export default {
      * @param value 输入值
      * @param loader 加载器类型
      */
-    attachMoreFilter (value, loader) {
+    async attachMoreFilter (value, loader) {
       if (value == null || (value = value.trim()).length === 0) {
         this.$set(this.more, `${loader}Options`, [])
         return
@@ -49,23 +51,69 @@ export default {
       this.attachMoreLoading = true
       const moreLoader = this.attachMoreLoader[loader]
       moreLoader.keyword = value
-      dibootApi.post('/common/attachMoreFilter', moreLoader).then(res => {
-        if (res.ok) {
-          this.$set(this.more, `${loader}Options`, res.data)
-        } else {
-          this.$notification.error({
-            title: '获取选项数据失败',
-            message: res.msg
-          })
-        }
-        this.attachMoreLoading = false
-      }).catch((err) => {
-        this.attachMoreLoading = false
+      this.$set(this.more, `${loader}Options`, await this.LoadAttachMore(moreLoader))
+      this.attachMoreLoading = false
+    },
+    /**
+     * 加载AttachMore
+     *
+     * @param moreLoader 加载器
+     * @param node 节点（可空）
+     */
+    async LoadAttachMore (moreLoader, node = {}) {
+      if (moreLoader.disabled) {
+        return
+      }
+      const build = item => item ? `/${item}` : ''
+      const res = await dibootApi.post('/common/attachMoreFilter' + build(node.type) + build(node.value), moreLoader)
+      if (res.ok) {
+        return res.data
+      } else {
         this.$notification.error({
           title: '获取选项数据失败',
-          message: err.msg
+          message: res.msg
         })
+      }
+    },
+    /**
+     * 异步加载更多
+     *
+     * @param node 当前节点
+     * @param loader 加载器名称
+     */
+    async lazyLoadMore (node, loader) {
+      const targetNode = node[node.length - 1]
+      targetNode.loading = true
+      const moreLoader = this.attachMoreLoader[loader]
+      const dataLsit = await this.LoadAttachMore(moreLoader, targetNode) || []
+      dataLsit.length === 0 && moreLoader.next != null && this.$set(targetNode, 'disabled', true)
+      targetNode.children = dataLsit.map(e => {
+        e.isLeaf = e.isLeaf || false
+        return e
       })
+      targetNode.loading = false
+      this.$set(this.more, `${loader}Options`, [...this.more[`${loader}Options`]])
+    },
+    /**
+     * 控制相关选项的获取
+     *
+     * @param value 选项值
+     * @param control 控制器名称
+     */
+    controlRelationOptions (value, control) {
+      const controlItem = this.control[control]
+      const isNull = value == null || value.length === 0
+      const execute = async ({ name, loader, condition, lazy }) => {
+        const moreLoader = this.attachMoreLoader[loader]
+        moreLoader.disabled = isNull
+        moreLoader.condition == null && (moreLoader.condition = {})
+        moreLoader.condition[condition] = value
+        const obj = {}
+        obj[name] = undefined
+        this.form.setFieldsValue(obj)
+        !lazy && this.$set(this.more, `${loader}Options`, isNull ? [] : await this.LoadAttachMore(moreLoader))
+      }
+      controlItem instanceof Array ? controlItem.forEach(item => execute(item)) : execute(controlItem || {})
     }
   },
   async mounted () {
