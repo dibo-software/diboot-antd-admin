0051e3dfd10c3054ec1e1dd143186f3cb5b8e811
wind <1103642893@qq.com>
2022-05-19 16:24:26 +0800

    - 移除打包cdn

--- vue.config.js
+++ vue.config.js
@@ -17,25 +17,25 @@ function getGitHash () {
   return 'unknown'
 }
 
-const isProd = process.env.NODE_ENV === 'production'
-
-const assetsCDN = {
-  // webpack build externals
-  externals: {
-    vue: 'Vue',
-    'vue-router': 'VueRouter',
-    vuex: 'Vuex',
-    axios: 'axios'
-  },
-  css: [],
-  // https://unpkg.com/browse/vue@2.6.10/
-  js: [
-    '//cdn.jsdelivr.net/npm/vue@2.6.10/dist/vue.min.js',
-    '//cdn.jsdelivr.net/npm/vue-router@3.1.3/dist/vue-router.min.js',
-    '//cdn.jsdelivr.net/npm/vuex@3.1.1/dist/vuex.min.js',
-    '//cdn.jsdelivr.net/npm/axios@0.19.0/dist/axios.min.js'
-  ]
-}
+// const isProd = process.env.NODE_ENV === 'production'
+//
+// const assetsCDN = {
+//   // webpack build externals
+//   externals: {
+//     vue: 'Vue',
+//     'vue-router': 'VueRouter',
+//     vuex: 'Vuex',
+//     axios: 'axios'
+//   },
+//   css: [],
+//   // https://unpkg.com/browse/vue@2.6.10/
+//   js: [
+//     '//cdn.jsdelivr.net/npm/vue@2.6.10/dist/vue.min.js',
+//     '//cdn.jsdelivr.net/npm/vue-router@3.1.3/dist/vue-router.min.js',
+//     '//cdn.jsdelivr.net/npm/vuex@3.1.1/dist/vuex.min.js',
+//     '//cdn.jsdelivr.net/npm/axios@0.19.0/dist/axios.min.js'
+//   ]
+// }
 
 // vue.config.js
 const vueConfig = {
@@ -49,9 +49,9 @@ const vueConfig = {
         GIT_HASH: JSON.stringify(getGitHash()),
         BUILD_DATE: buildDate
       })
-    ],
+    ]
     // if prod, add externals
-    externals: isProd ? assetsCDN.externals : {}
+    // externals: isProd ? assetsCDN.externals : {}
   },
 
   chainWebpack: (config) => {
@@ -76,12 +76,12 @@ const vueConfig = {
 
     // if prod is on
     // assets require on cdn
-    if (isProd) {
-      config.plugin('html').tap(args => {
-        args[0].cdn = assetsCDN
-        return args
-      })
-    }
+    // if (isProd) {
+    //   config.plugin('html').tap(args => {
+    //     args[0].cdn = assetsCDN
+    //     return args
+    //   })
+    // }
   },
 
   css: {
